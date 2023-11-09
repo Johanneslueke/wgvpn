ade66e9d09b63d14245cf5d1c05bda76e34bd0fd
Johannes Lüke <johannes.lueke@protonmail.com>
2023-11-09 15:45:58 +0100

    refactor(wgbindraw-sys): improve code generation of binding

--- wgbind/src/lib.rs
+++ wgbind/src/lib.rs
@@ -89,7 +89,7 @@ pub fn list_device_names() -> Option<Vec<String>> {
     Some(Vec::from_iter(result.split_terminator('\0').map(|x| String::from(x))))
 }
 
-/// Add a wireguard device
+/// Add a wireguard network interface device
 /// 
 /// What is a device? Simply just a network interface. A user could instead simply create
 /// this interface manually via:
@@ -124,6 +124,8 @@ pub fn add_device(device_name: &str) -> Result<(),std::io::Error>{
     Err(std::io::Error::last_os_error())
 
 }
+
+/// .
 pub fn delete_device(device_name: &str) -> Result<(),std::io::Error>{
     let name = CString::new(device_name).unwrap().into_raw().cast() as *const ::std::os::raw::c_char ;
     let result = unsafe{ wg_del_device(name)};
@@ -136,6 +138,7 @@ pub fn delete_device(device_name: &str) -> Result<(),std::io::Error>{
 
 }
 
+/// .
 pub fn get_device(device_name: &str) -> Result<wg_device,std::io::Error>{
     let name = CString::new(device_name).unwrap().into_raw().cast() as *const ::std::os::raw::c_char ;
     //let structsize = std::mem::size_of::<wg_device>()+ std::mem::size_of::<wg_peer>()*2;
@@ -157,9 +160,218 @@ pub fn get_device(device_name: &str) -> Result<wg_device,std::io::Error>{
     Err(std::io::Error::last_os_error())
 
 }
+
+/// # WireguardDevice
+/// 
+/// holds configuration data for a Wireguard Device.
+/// if this object drops, the associated strings get 
+/// lost, if the wireguard implementation depends on these
+/// string values, that might cause significant problems !!!
+/// 
+/// 
+#[derive(Debug)]
+pub struct WireguardDevice {
+    raw_device : Box<::core::ffi::c_void>,
+    name : &'static str,
+    flags: wg_device_flags,
+    fwmark: u32,
+    private_key: Option<&'static str>,
+    public_key: Option<&'static str>,
+}
+
  
+
+impl WireguardDevice {
+    pub fn new(device : Box<wg_device>,name: &'static str, flags: wg_device_flags, fwmark: u32, private_key: Option<&'static str>, public_key: Option<&'static str>) -> Self { 
+        Self { raw_device: device, name, flags, fwmark, private_key, public_key,  } 
+    }
+
+    pub fn private_key(&self) -> Option<&str> {
+        self.private_key
+    }
+
+    pub fn public_key(&self) -> Option<&str> {
+        self.public_key
+    }
+
+    pub fn name(&self) -> &str {
+        self.name
+    }
+
+    pub fn set_name(&mut self, name: &'static str) {
+        self.name = name;
+    }
+
+    pub fn flags(&self) -> wg_device_flags {
+        self.flags
+    }
+
+    pub fn set_flags(&mut self, flags: wg_device_flags) {
+        self.flags = flags;
+    }
+
+    pub fn fwmark(&self) -> u32 {
+        self.fwmark
+    }
+
+    pub fn set_fwmark(&mut self, fwmark: u32) {
+        self.fwmark = fwmark;
+    }
+}
+
+impl Into<wg_device> for WireguardDevice{
+    fn into(self) -> wg_device {
+        todo!()
+    }
+}
+
+impl Into<WireguardDevice> for wg_device{
+    fn into(self) -> WireguardDevice {
+        todo!()
+    }
+}
+
+
+impl Into<*mut wg_device> for WireguardDevice{
+    fn into(self) -> *mut wg_device {
+        todo!()
+    }
+}
+
+impl Into<&mut WireguardDevice> for wg_device {
+    fn into(self) -> &'static mut WireguardDevice {
+        todo!()
+    }
+}
+
+
+impl Drop for WireguardDevice {
+    fn drop(&mut self) {
+        free_device(self);
+    }
+}
+
+
+
+impl WireguardControl for WireguardDevice{
+    fn create_interface(&self)-> Result<(),std::io::Error> {
+        return add_device(self.name)
+    }
+
+    fn remove_interface(&self) -> Result<(),std::io::Error> {
+        todo!()
+    }
+
+    fn update_device(&mut self) -> Result<(),std::io::Error> {
+        let result = get_device(self.name);
+        *self = result.unwrap().into();
+
+        Ok(())
+    }
+
+    fn refresh_device(&mut self) -> Result<(), std::io::Error> {
+        
+        set_device(self)
+    }
+}
+
+trait WireguardControl   {
+    fn create_interface(&self) -> Result<(),std::io::Error>;
+    fn remove_interface(&self) -> Result<(),std::io::Error>;
+
+    /// writes to the kernal 
+    fn update_device(&mut self) -> Result<(),std::io::Error>;
+
+    /// Read from the kernel
+    fn refresh_device(&mut self) -> Result<(), std::io::Error>;
+}
+
+/// Set a new wireguard device - not a network interface
+/// 
+/// A wireguard device is the corresponding kernel object. Values of this 
+/// device have an effect on the existing wireguard network interface device!
+/// 
+/// # Arguments
+/// 
+/// * `device` - Wireguard configuration data
+/// 
+/// 
+pub fn set_device(device : &mut WireguardDevice) -> Result<(), std::io::Error> {
+
+    let raw_ptr = &* device.raw_device as *const ::core::ffi::c_void;
+
+    // If the wireguard device has already a raw device then simply us that pointer 
+    // only if the pointer is NULL skip this and create new raw device
+    if raw_ptr.is_null() == false{
+        let error = unsafe { wg_set_device(raw_ptr.cast_mut() as *mut wg_device )};
+    
+        if error != 0 {
+            return Err(std::io::Error::last_os_error());
+        }
+
+    }
+    
+    // transform the name from &'static str into [i8;16]. The way to get there seems fucked up
+    // not sure if this the best way
+    let devicename = unsafe {
+        let devicename = CString::new(device.name).expect("CString::new failed");
+        let devicename = std::ffi::CStr::from_bytes_with_nul(devicename.to_bytes_with_nul()).expect("CStr::from_bytes_with_null failed");
+        let devicename = std::slice::from_raw_parts( devicename.as_ptr(), 16).as_ptr();
+        let devicename = *std::mem::transmute::<*const i8,&[i8;16]>(devicename);
+
+        devicename
+    };
+
+    // create 2 NULL ponter 
+    let (firstpeer,lastpeer) = {
+        let firstpeer =std::ptr::null::<*mut wg_peer>() as *mut wg_peer;
+        let lastpeer = std::ptr::null::<*mut wg_peer>() as *mut wg_peer;
+
+        (firstpeer,lastpeer)
+    };
+
+    // Create on the Heap the wg_device. The pointer to the heap will be forwarded into
+    // into wireguard c implementation
+    let  wgdevice  =  Box::new(wg_device { 
+        name: devicename,
+        ifindex: 0, 
+        flags: device.flags, 
+         
+        fwmark: device.fwmark, 
+        listen_port: 51820,
+       
+        first_peer: firstpeer,
+        last_peer: lastpeer,   
+        private_key: Default::default(),
+        public_key:  Default::default(),
+
+
+        //ignore, just for padding purposes
+        __bindgen_padding_0:  Default::default(),
+    });
+
+    // Because the pointer is of type c_void, the destructor of Box has no effect on the value
+    // behind the pointer
+    let raw_ptr = &* wgdevice as *const wg_device;
+    let handle = raw_ptr as *mut ::core::ffi::c_void;
+    device.raw_device = unsafe { Box::from_raw(handle) };
+
+    let error = unsafe { wg_set_device(raw_ptr.cast_mut() )};
+
+    if error != 0 {
+        return Err(std::io::Error::last_os_error());
+    }
+
+    Ok(())
+   
+}
  
- 
+pub fn free_device(device: &mut WireguardDevice) {
+    unsafe{
+        let wireguard_device = *device;
+         wg_free_device((wireguard_device).into())
+    }
+}
 
 #[cfg(test)]
 mod tests { 
--- wgbindraw-sys/build.rs
+++ wgbindraw-sys/build.rs
@@ -1,69 +1,66 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+
 use std::env;
 use std::path::PathBuf;
 
-fn main() {
+const libctypes: &[&str] = &[
+    "in_addr",
+    "in6_addr",
+    "sockaddr",
+    "sockaddr_in",
+    "sockaddr_in6",
+    "timespec",
+    
+];
 
+const ignoretypes: &[&str] = &[
+    "in6_addr__bindgen_ty_1",
+    "sa_family_t",
+    "in_addr_t",
+    "in_port_t"
+    ];
+
+fn main() {
     //println!("cargo:rustc-link-lib=clib");
     println!("cargo:rerun-if-changed=wg/wireguard.h");
 
-    cc::Build::new()
-        .file("wg/wireguard.c")
-        .compile("wireguard.a");
-
-    let bindings = bindgen::Builder::default()
+    let mut bindings = bindgen::Builder::default()
         .header("wg/wireguard.h")
+        .impl_debug(true)
         .clang_arg("-fretain-comments-from-system-headers")
         .sort_semantically(true)
         .generate_comments(true)
         .generate_block(true)
-        .generate_cstr(true) 
-        .record_matches(true)
-        .explicit_padding(true)
-        .translate_enum_integer_types(true)
+        .generate_cstr(true)
+        //.explicit_padding(true)
+        //.translate_enum_integer_types(true)
         .enable_function_attribute_detection()
-        .emit_builtins()
-        .emit_ir()
-        .emit_clang_ast()
-        .emit_ir_graphviz("./graphviz.gz")
         .wrap_unsafe_ops(true)
         .use_core()
-
-        .anon_fields_prefix("wg_anonym_")
         .array_pointers_in_arguments(true)
-
-        . must_use_type("int") 
-        . must_use_type("bool")
-        . must_use_type("char")
-        . must_use_type(r"char \*")
-
-        .allowlist_function("wg_set_device")
-        .allowlist_function("wg_get_device")
-        .allowlist_function("wg_add_device")
-        .allowlist_function("wg_del_device")
-        .allowlist_function("wg_free_device")
-        .allowlist_function("wg_list_device_names")
-        .allowlist_function("wg_key_to_base64")
-        .allowlist_function("wg_key_from_base64")
-        .allowlist_function("wg_key_is_zero")
-        .allowlist_function("wg_generate_public_key")
-        .allowlist_function("wg_generate_private_key")
-        .allowlist_function("wg_generate_preshared_key")
-
-
-
-
-        .allowlist_type("wg_key")
-        .allowlist_type("wg_device")
-        .allowlist_type("wg_key_b64_string")
-        .bitfield_enum("wg_device_flags")
+        // .must_use_type("int")
+        // .must_use_type("bool")
+        // .must_use_type("char")
+        // .must_use_type(r"char \*")
+        .allowlist_function("wg_.*")
         .bitfield_enum("wg_peer_flags")
-        
-        
-    
-
+        .bitfield_enum("wg_device_flags")
+         
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .generate()
-        .expect("Couldn't generate bindings");
+        .raw_line("extern crate libc;");;
+
+    for libc_type in libctypes {
+        bindings = bindings
+            .blocklist_type(libc_type)
+            .raw_line(format!("use libc::{};", libc_type));
+    }
+
+    for useless_type in ignoretypes {
+        bindings = bindings.blocklist_type(useless_type);
+    }
+
+    let bindings = bindings.generate().expect("Couldn't generate bindings");
 
     let out_path = PathBuf::from("./src");
     bindings
@@ -72,6 +69,14 @@ fn main() {
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
-    .write_to_file(out_path.join("bindings.rs"))
-    .expect("Could not write to file");
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Could not write to file");
+
+    cc::Build::new()
+        .file("wg/wireguard.c")
+        .warnings(true)
+        .extra_warnings(true)
+        .warnings_into_errors(true)
+        .flag_if_supported("-Wno-unused-parameter")
+        .compile("wireguard.a");
 }
